11aca3cd1e2c90b4457bc4972e8255dfc7110a26
hakaton-2019 <52372938+hakaton-2019@users.noreply.github.com>
2019-06-30 15:15:21 +0500

    Add files via upload

---
+++ www/precache-manifest.cdde085bde0e6c6bf35cde86296133a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e948931e4b41e05f9a4e85cf324d35fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2010093da63078797f0d",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c0e1d06d1da2814bf0c1",
+    "url": "/static/js/2.074a585c.chunk.js"
+  },
+  {
+    "revision": "2010093da63078797f0d",
+    "url": "/static/js/main.bbc9c04e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
